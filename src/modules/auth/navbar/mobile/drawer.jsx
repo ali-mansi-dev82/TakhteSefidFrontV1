fd224dc31c65d38512f2399c0733cc8b6080c5e1
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-07-20 21:22:51 +0330

    add drawer

--- src/modules/auth/navbar/mobile/drawer.jsx
+++ src/modules/auth/navbar/mobile/drawer.jsx
@@ -3,13 +3,18 @@ import { Button, Drawer } from "@mui/material";
 
 import { ReactComponent as Hamberger } from "../../../../assets/icons/elipes.svg";
 import { ReactComponent as X } from "../../../../assets/icons/x.svg";
+import { ReactComponent as Chevrow } from "../../../../assets/icons/chevron-down.svg";
 import { ReactComponent as Logo } from "../../../../assets/Logo.svg";
 
 const MobileDrawer = () => {
   const [open, setOpen] = React.useState(false);
+  const [dropOpen, setDropOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
+  };
+  const toggleDrop = (newOpen) => () => {
+    setDropOpen(newOpen);
   };
   return (
     <>
@@ -40,17 +45,33 @@ const MobileDrawer = () => {
               ورود
             </Button>
           </div>
-          <ul className="flex flex-col gap-8 text-sm text-gray-700 mt-10">
-            <li className="">
-              <span className="">خدمات</span>
+          <ul className="flex flex-col gap-2 text-sm text-gray-700 mt-10 group">
+            <li className="py-3 w-full" onClick={toggleDrop(!dropOpen)}>
+              <div className="w-full inline-flex justify-between">
+                <span className="">خدمات</span>
+                <Chevrow />
+              </div>
+              {dropOpen && (
+                <ul className="text-gray-500">
+                  <li className="hover:bg-primary-10 hover:text-primary-80 text-sm px-4 py-3">
+                    برگزاری کلاس آنلاین
+                  </li>
+                  <li className="hover:bg-primary-10 hover:text-primary-80 text-sm px-4 py-3">
+                    برگزاری وبینار
+                  </li>
+                  <li className="hover:bg-primary-10 hover:text-primary-80 text-sm px-4 py-3">
+                    برگزاری جلسه آنلاین (وب کنفرانس)
+                  </li>
+                </ul>
+              )}
             </li>
-            <li className="">
+            <li className="py-3 ">
               <span className="">تعرفه ها</span>
             </li>
-            <li className="">
+            <li className="py-3 ">
               <span className="">تماس با ما</span>
             </li>
-            <li className="">
+            <li className="py-3 ">
               <span className="">درباره ما</span>
             </li>
           </ul>
--- src/modules/auth/navbar/mobile/index.jsx
+++ src/modules/auth/navbar/mobile/index.jsx
@@ -10,10 +10,8 @@ const index = () => {
   return (
     <div className="flex flex-row justify-center items-center bg-white border-b border-gray-200 h-[65px] fixed top-0 left-0 right-0 z-50">
       <MainContainer className="flex justify-between items-center">
-        <div className="flex gap-2">
-          <Drawer />
-          <Logo />
-        </div>
+        <Drawer />
+        <Logo />
         <Button size="small" variant="contained">
           ثبت نام
         </Button>
