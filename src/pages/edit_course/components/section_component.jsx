ba62b3863d943da7a4aa8621be967b93ac64c067
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-07-31 17:33:22 +0330

    cr update course page ui

---
+++ src/pages/edit_course/components/section_component.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import ActivityComponent from "./activity_component";
+import { ReactComponent as Trash } from "../../../assets/icons/trash.svg";
+import { ReactComponent as PenLine } from "../../../assets/icons/pen-line.svg";
+
+const SectionComponent = ({ title, activity }) => {
+  const [open, setOpen] = useState(true);
+  return (
+    <ul className="flex flex-col">
+      <li
+        className="inline-flex justify-between items-center px-4 py-4 border-b border-t border-gray-300 bg-gray-50"
+        onClick={setOpen.bind(this, !open)}
+      >
+        <span className="text-base text-primary-60 YekanBakhFaNum">
+          {title}
+        </span>
+        <div className="inline-flex gap-2">
+        <button className="inline-flex w-9 h-9 items-center justify-center bg-primary-0 text-primary-60 rounded-md">
+          <span className="inline-flex w-4">
+            <PenLine />
+          </span>
+        </button>
+        <button className="inline-flex w-9 h-9 items-center justify-center bg-primary-0 text-primary-60 rounded-md">
+          <span className="inline-flex w-4">
+            <Trash />
+          </span>
+        </button>
+      </div>
+      </li>
+      <ul className={`flex flex-col h-0 overflow-hidden ${open && `h-auto py-2`} duration-300 ease-in-out	 `}>
+        {activity.map((activity) => (
+          <ActivityComponent {...activity} />
+        ))}
+      </ul>
+    </ul>
+  );
+};
+
+export default SectionComponent;
