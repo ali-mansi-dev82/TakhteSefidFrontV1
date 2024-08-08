fcf79c2b92000b8ecff25b8c56e9c30e6a3b103e
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-08-08 07:32:21 +0330

    add middleware

--- src/app/store.js
+++ src/app/store.js
@@ -5,20 +5,23 @@ import { authApi } from "../services/authService";
 import { activityApi } from "../services/activityService";
 import { attendApi } from "../services/attendCourseService";
 import { myCourseApi } from "../services/myCourseService";
+import { courseApi } from "../services/courseService";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
     [activityApi.reducerPath]: activityApi.reducer,
-    [attendApi.reducerPath]: attendApi.reducer,
     [myCourseApi.reducerPath]: myCourseApi.reducer,
+    [courseApi.reducerPath]: courseApi.reducer,
+    [attendApi.reducerPath]: attendApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(authApi.middleware)
       .concat(activityApi.middleware)
       .concat(attendApi.middleware)
+      .concat(courseApi.middleware)
       .concat(myCourseApi.middleware),
 });
 export default store;
